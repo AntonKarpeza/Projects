3258f72af89eac2adc6cd57fc76c48a888ef91c1
Anton Karpeza <karpezaanton@gmail.com>
2018-04-06 10:56:59 +0300

    add theatre to wdev

--- wdev/app/js/common.js
+++ wdev/app/js/common.js
@@ -137,9 +137,9 @@ $(window).on("load", function(){
 
 // MAP
 function myMap() {
-    var myCenter = new google.maps.LatLng(55.53,28.65);
+    var myCenter = new google.maps.LatLng(53.91,27.45);
     var mapCanvas = document.getElementById("googleMap");
-    var mapOptions = {center: myCenter, zoom: 7};
+    var mapOptions = {center: myCenter, zoom: 7, mapTypeControl: false, fullscreenControl: false};
     var map = new google.maps.Map(mapCanvas, mapOptions);
     var marker = new google.maps.Marker({position:myCenter});
     marker.setMap(map);
